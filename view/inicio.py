108822f503cb190cb4665755d1b639facd71de00
christopher <teoriadelcaos.cl@gmail.com>
2020-07-26 22:10:55 -0500

    Tercera version

--- view/inicio.py
+++ view/inicio.py
@@ -6,9 +6,9 @@ window.configure(background="black")
 eti_ini = Label(window,text="Aplicación SRF",fg="white",bg="black",font=("AlternateGothic2 BT",45)).place(x=160,y=10)
 eti_ini_emp=Label(window,text="APP DevMar",fg="white",bg="black",font=("Monotype Corsiva",)).place(x=500,y=360)
 
-botonI= Button(window,text="Iniciar reconocimiento",fg="black",bg ="yellow").place(x=50,y=130)
+botonI= Button(window,text="Iniciar video",fg="black",bg ="yellow").place(x=50,y=130)
 botonI1= Button(window,text="      Capturar rostro      ",fg="black").place(x=50,y=200)
-botonI2= Button(window,text="Entrenamiento rostro ",fg="black").place(x=50,y=270)
+botonI2= Button(window,text="Reconocimiento rostro ",fg="black").place(x=50,y=270)
 CajaI = Entry(window,text="").place(x=260,y=140)
 
 
